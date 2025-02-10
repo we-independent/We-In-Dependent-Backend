230886ea260627438e80387824c3b43a1947620c
enhuili <lexi05732@gmail.com>
2025-02-09 23:48:59 -0500

    feat: 添加 @MapperScan 注解

--- weindependent-app/src/main/java/com/weindependent/app/AppApplication.java
+++ weindependent-app/src/main/java/com/weindependent/app/AppApplication.java
@@ -1,9 +1,11 @@
 package com.weindependent.app;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.weindependent.app.database.mapper.weindependent")
 public class AppApplication {
 
     public static void main(String[] args) {
