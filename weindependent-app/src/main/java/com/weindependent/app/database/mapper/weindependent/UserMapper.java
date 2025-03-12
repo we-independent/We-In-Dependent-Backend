15cf0f2ba0e55f99a6637859d77a6798daa5bec2
enhuili <lexi05732@gmail.com>
2025-03-12 08:26:47 -0400

    fix:修复UserMapper和UserService中的sql查询问题

--- weindependent-app/src/main/java/com/weindependent/app/database/mapper/weindependent/UserMapper.java
+++ weindependent-app/src/main/java/com/weindependent/app/database/mapper/weindependent/UserMapper.java
@@ -5,9 +5,12 @@ import org.apache.ibatis.annotations.Mapper;
 
 import java.util.List;
 import java.util.Map;
+import org.apache.ibatis.annotations.*;
 
 @Mapper
 public interface UserMapper {
     UserDO query(Map<String, String> paramMap);
     List<UserDO> getAllUsers();
+    UserDO findByAccount(String account);
+    int insert(UserDO user);
 }
