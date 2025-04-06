515c5f5eb9ececaf78322f476aab4ae8f6c54337
hurelyyu@gmail.com <yaqunyu@uw.edu>
2025-04-05 17:56:08 -0700

    Revert "cleaned code"
    This reverts commit db1610653579d46ba01803508a938513696f41a5.

---
+++ weindependent-app/src/main/java/com/weindependent/app/controller/dashboard/TagController.java
@@ -0,0 +1,98 @@
+package com.weindependent.app.controller.dashboard;
+
+import java.util.Map;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+import com.github.pagehelper.PageInfo;
+import com.weindependent.app.annotation.SignatureAuth;
+import com.weindependent.app.database.dataobject.TagDO;
+import com.weindependent.app.dto.CategoryQry;
+import com.weindependent.app.dto.TagQry;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.weindependent.app.service.ITagService;
+
+
+/**
+ * 标签Controller
+ * 
+ * @author chistina
+ *    2025-03-23
+ */
+@Tag(name = "标签管理")
+@RestController
+@RequestMapping("/dashboard/tag")
+public class TagController
+{
+    @Autowired
+    private ITagService tagService;
+
+    /**
+     * 查询标签列表
+     */
+    @SignatureAuth
+    @Operation(summary = "查询标签列表")
+    @GetMapping("/list")
+    public PageInfo<TagDO> list(@RequestBody TagQry tagQry)
+    {
+
+        return  tagService.selectTagList(tagQry);
+    }
+
+
+
+    /**
+     * 查询标签详细信息
+     */
+    @SignatureAuth
+    @Operation(summary = "查询标签详细信息")
+    @GetMapping(value = "/{id}")
+    public TagDO getInfo(@PathVariable("id") Integer id)
+    {
+        return tagService.selectTagById(id);
+    }
+
+    /**
+     * 新增标签
+     */
+    @SignatureAuth
+    @Operation(summary = "新增标签")
+    @PostMapping
+    public boolean add(@RequestBody TagDO tag)
+    {
+        return tagService.insertTag(tag) > 0;
+    }
+
+    /**
+     * 修改标签
+     */
+    @SignatureAuth
+    @Operation(summary = "修改标签")
+    @PutMapping
+    public boolean edit(@RequestBody TagDO tag)
+    {
+        return tagService.updateTag(tag) > 0;
+    }
+
+    /**
+     * 删除标签
+     */
+    @SignatureAuth
+    @Operation(summary = "删除标签")
+	@DeleteMapping("/{ids}")
+    public boolean remove(@PathVariable Integer[] ids)
+    {
+        return tagService.deleteTagByIds(ids) > 0;
+    }
+}
---
+++ weindependent-app/src/main/java/com/weindependent/app/database/mapper/dashboard/BlogPdfMapper.java
@@ -0,0 +1,65 @@
+package com.weindependent.app.database.mapper.dashboard;
+
+import java.util.List;
+
+import com.weindependent.app.database.dataobject.BlogPdfDO;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * 博客文章pdfMapper接口
+ * dashboard 查询包含 isDeleted=1 的 record
+ * 
+ * @author christina
+ *    2025-03-23
+ */
+@Mapper
+public interface BlogPdfMapper 
+{
+    /**
+     * 查询博客文章pdf
+     * 
+     * @param id 博客文章pdf主键
+     * @return 博客文章pdf
+     */
+    public BlogPdfDO selectBlogPdfById(Integer id);
+
+    /**
+     * 查询博客文章pdf列表
+     * 
+     * @param blogPdf 博客文章pdf
+     * @return 博客文章pdf集合
+     */
+    public List<BlogPdfDO> selectBlogPdfList(BlogPdfDO blogPdf);
+
+    /**
+     * 新增博客文章pdf
+     * 
+     * @param blogPdf 博客文章pdf
+     * @return 结果
+     */
+    public int insertBlogPdf(BlogPdfDO blogPdf);
+
+    /**
+     * 修改博客文章pdf
+     * 
+     * @param blogPdf 博客文章pdf
+     * @return 结果
+     */
+    public int updateBlogPdf(BlogPdfDO blogPdf);
+
+    /**
+     * 删除博客文章pdf信息，, 软删除
+     * 
+     * @param id 博客文章pdf主键
+     * @return 结果
+     */
+    public int deleteBlogPdfById(Integer id);
+
+    /**
+     * 批量删除博客文章pdf信息，, 软删除
+     * 
+     * @param ids 需要删除的数据主键集合
+     * @return 结果
+     */
+    public int deleteBlogPdfByIds(Integer[] ids);
+}
