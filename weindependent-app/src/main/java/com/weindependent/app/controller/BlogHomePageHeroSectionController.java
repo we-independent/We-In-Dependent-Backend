c7f5f3c430098dadcd37c37e3922f23c3961cd28
Shuang <xies5848@gmail.com>
2025-04-22 12:59:52 -0400

    fix(controller): 补充 EditorPickVO 的 import

--- weindependent-app/src/main/java/com/weindependent/app/controller/BlogHomePageHeroSectionController.java
+++ weindependent-app/src/main/java/com/weindependent/app/controller/BlogHomePageHeroSectionController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 import java.util.List;
+import com.weindependent.app.vo.EditorPickVO;
 
 @Tag(name = "Blog首页Hero区域")
 @RestController
