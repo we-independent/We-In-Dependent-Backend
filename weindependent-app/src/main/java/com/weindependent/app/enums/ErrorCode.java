f6df284eef2372356e6788964c2888e4c35526d5
hurelyyu@gmail.com <yaqunyu@uw.edu>
2025-04-22 17:51:21 -0700

    fix:更改字体流的获取方式

--- weindependent-app/src/main/java/com/weindependent/app/enums/ErrorCode.java
+++ weindependent-app/src/main/java/com/weindependent/app/enums/ErrorCode.java
@@ -15,8 +15,8 @@ public enum ErrorCode {
     BLOG_NOT_EXIST(-4, "Blog does not exist"),
     USERNAME_PASSWORD_ERROR(401, "Invalid username or password"),
     UNAUTHORIZED_ACCESS(403, "You are not allowed to access this resource."),
-    RELATED_ARTICLE_FETCH_FAILED(201, "Fail to Get Related Articles");
-
+    RELATED_ARTICLE_FETCH_FAILED(201, "Fail to Get Related Articles"),
+    FONT_NOT_EXIST(-5, "Font not exist");
 
     private int code;
     private final String title;
--- weindependent-app/src/main/java/com/weindependent/app/service/impl/BlogPdfExportServiceImpl.java
+++ weindependent-app/src/main/java/com/weindependent/app/service/impl/BlogPdfExportServiceImpl.java
@@ -6,12 +6,15 @@ import java.io.ByteArrayOutputStream;
 import java.io.File;
 
 import com.weindependent.app.database.mapper.weindependent.BlogPdfExportMapper;
+import com.weindependent.app.enums.ErrorCode;
+import com.weindependent.app.exception.ResponseException;
 import com.weindependent.app.database.dataobject.BlogArticleDO;
 import com.weindependent.app.utils.HtmlSanitizerUtil;
 import com.openhtmltopdf.outputdevice.helper.BaseRendererBuilder;
 import com.openhtmltopdf.pdfboxout.PdfRendererBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import java.io.InputStream;
 
 import com.weindependent.app.service.IBlogPdfExportService;
 import org.thymeleaf.TemplateEngine;
@@ -45,10 +48,18 @@ public class BlogPdfExportServiceImpl implements IBlogPdfExportService  {
 
         try (ByteArrayOutputStream outputStream = new ByteArrayOutputStream()) {
             PdfRendererBuilder builder = new PdfRendererBuilder();
-            String fontPath = this.getClass().getClassLoader().getResource("fonts/NotoSansSC-Regular.ttf").getPath();
+            try (InputStream fontStream = this.getClass().getClassLoader().getResourceAsStream("fonts/NotoSansSC-Regular.ttf")) {
+                System.out.println("字体流是否获取成功：" + (fontStream != null));
+                if (fontStream != null) {
+                    builder.useFont(() -> fontStream, "Noto Sans SC", 400, BaseRendererBuilder.FontStyle.NORMAL, true);
+                } else {
+                    throw new ResponseException(ErrorCode.FONT_NOT_EXIST.getCode(), "字体不存在");
+                }
+            }
+            // String fontPath = this.getClass().getClassLoader().getResource("fonts/NotoSansSC-Regular.ttf").getPath();
             builder.useFastMode();
             builder.withHtmlContent(safeHtml, null);
-            builder.useFont(new File(fontPath), "Noto Sans SC", 400, BaseRendererBuilder.FontStyle.NORMAL, true);
+            // builder.useFont(new File(fontStream), "Noto Sans SC", 400, BaseRendererBuilder.FontStyle.NORMAL, true);
             builder.toStream(outputStream);
             builder.run();
             return outputStream.toByteArray();
