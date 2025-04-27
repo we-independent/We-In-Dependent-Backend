3a7eefa83e9b3cc5b6ab498f7e5738afd1d1e001
christinazou <christinazou@users.noreply.github.com>
2025-04-27 20:14:00 +0800

    fix: added VO for api to get all not deleted tag

---
+++ weindependent-app/src/main/java/com/weindependent/app/vo/TagCategoryVO.java
@@ -0,0 +1,11 @@
+package com.weindependent.app.vo;
+
+import lombok.Data;
+
+@Data
+public class TagCategoryVO {
+    private Integer tagId;
+    private String TagName;
+    private Integer categoryId;
+    private String categoryName;
+}
