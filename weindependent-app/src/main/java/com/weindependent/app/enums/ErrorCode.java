ccdc037f1cf65d72d969836b763b84482c8d8b48
Elly Zhang <ranxinzh@gmail.com>
2025-04-28 19:42:11 -0500

    save event

--- weindependent-app/src/main/java/com/weindependent/app/enums/ErrorCode.java
+++ weindependent-app/src/main/java/com/weindependent/app/enums/ErrorCode.java
@@ -17,7 +17,8 @@ public enum ErrorCode {
     UNAUTHORIZED_ACCESS(403, "You are not allowed to access this resource."),
     FONT_NOT_EXIST(-5, "Font not exist"),
     RELATED_ARTICLE_FETCH_FAILED(-5, "Fail to Get Related Articles"), 
-    UNKONWN_CATEGORY(-6, "Unknown Category");
+    UNKONWN_CATEGORY(-6, "Unknown Category"),
+    EVENT_NOT_EXIST(-7, "Event does not exist");
 
     
     private int code;
