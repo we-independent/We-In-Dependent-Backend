bb0237ffb5175b8e7a83235d7c707c5c1ca717f6
hurelyyu@gmail.com <yaqunyu@uw.edu>
2025-05-04 14:57:21 -0700

    refector:clean up pdf code, restructure pdf download and fallback logic

--- weindependent-app/src/main/java/com/weindependent/app/service/IBlogPdfDriveManagerService.java
+++ weindependent-app/src/main/java/com/weindependent/app/service/IBlogPdfDriveManagerService.java
@@ -3,5 +3,6 @@ package com.weindependent.app.service;
 import java.time.LocalDateTime;
 
 public interface IBlogPdfDriveManagerService {
-    String handlePdfDownload(Integer blogId, byte[] pdfBytes, Integer userId,int downloadCount, LocalDateTime now);
+    String handlePdfDownload(Integer blogId, byte[] pdfBytes, Integer userId,int downloadCount, LocalDateTime now, boolean forceUpload);
+    int getDownloadCount(Long blogId);
 }
