bf039202ec5420077877ee22c00d4aacabba06f4
wangjingjie <wangchingchieh.weindependent@gmail.com>
2025-05-07 10:09:32 -0400

    redo: still need signatureAuth for all api

--- weindependent-app/src/main/java/com/weindependent/app/controller/EventController.java
+++ weindependent-app/src/main/java/com/weindependent/app/controller/EventController.java
@@ -32,6 +32,7 @@ public class EventController {
 
 
     @Operation(summary = "Get event by ID")
+    @SignatureAuth
     @GetMapping("/{id}")
     public EventVO getEventById(@PathVariable Long id) {
         return IEventService.getEventById(id);
