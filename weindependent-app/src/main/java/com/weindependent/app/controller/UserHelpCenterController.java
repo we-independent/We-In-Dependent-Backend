1e6316e22c2e36249d7d8c1772b47831decddcce
hurelyyu@gmail.com <yaqunyu@uw.edu>
2025-05-13 16:08:38 -0700

    feat:新增Profile help center接口,实现发送给info信息和反馈给用户邮箱已收到Request信息,实现可查看用户历史help信息功能

---
+++ weindependent-app/src/main/java/com/weindependent/app/controller/UserHelpCenterController.java
@@ -0,0 +1,44 @@
+package com.weindependent.app.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.weindependent.app.dto.HelpCenterRequestQry;
+import com.weindependent.app.vo.HelpCenterRequestVO;
+import com.weindependent.app.service.IUserHelpCenterService;
+
+import cn.dev33.satoken.annotation.SaCheckLogin;
+import cn.dev33.satoken.stp.StpUtil;
+import io.swagger.v3.oas.annotations.Operation;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/user/profile/help")
+public class UserHelpCenterController {
+    
+    @Autowired
+    private IUserHelpCenterService userHelpCenterService;
+
+    @Operation(summary = "提交 User Profile Help Center 请求")
+    @SaCheckLogin
+    @PostMapping("/send")
+    public void sendHelp(@RequestBody HelpCenterRequestQry qry){
+        Long userId = StpUtil.getLoginIdAsLong();
+        userHelpCenterService.saveHelpRequest(userId, qry);
+    }
+
+    @Operation(summary = "获取User的 Help 历史记录请求")
+    @SaCheckLogin
+    @GetMapping("/history")
+    public List<HelpCenterRequestVO> getHelpRequestHistory(){
+        Long userId = StpUtil.getLoginIdAsLong();
+        return userHelpCenterService.getHelpRequestHistory(userId);
+    }
+    
+}
