4831276a265e5b1c83ea210a965982540e7006fc
ching-chieh-wang <immactavish@gmail.com>
2025-05-13 22:12:05 -0400

    feat: profile page event related apis

--- weindependent-app/src/main/java/com/weindependent/app/service/impl/EventServiceImpl.java
+++ weindependent-app/src/main/java/com/weindependent/app/service/impl/EventServiceImpl.java
@@ -2,6 +2,7 @@ package com.weindependent.app.service.impl;
 
 import cn.dev33.satoken.exception.NotLoginException;
 import cn.dev33.satoken.stp.StpUtil;
+import com.github.pagehelper.Page;
 import com.github.pagehelper.PageHelper;
 import com.weindependent.app.database.mapper.weindependent.EventMapper;
 import com.weindependent.app.enums.ErrorCode;
@@ -9,6 +10,7 @@ import com.weindependent.app.exception.ResponseException;
 import com.weindependent.app.service.IEventService;
 import com.weindependent.app.vo.event.EventVO;
 import com.weindependent.app.vo.event.RecentEventVO;
+import com.weindependent.app.vo.event.RecentEventVOs;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
@@ -42,11 +44,14 @@ public class EventServiceImpl implements IEventService {
         if(StpUtil.isLogin()){
             userId=StpUtil.getLoginIdAsInt();
         }
-        EventVO eventDO= eventMapper.getById(id,userId);
-        if (eventDO == null) {
+        EventVO eventVO= eventMapper.getById(id,userId);
+        if (eventVO == null) {
             throw new ResponseException(ErrorCode.EVENT_NOT_EXIST.getCode(),"Cannot find event");
         }
-        return eventDO;
+        if(userId != null){
+            eventMapper.recordUserViewEvent(userId,id);
+        }
+        return eventVO;
     }
 
     @Override
@@ -105,6 +110,50 @@ public class EventServiceImpl implements IEventService {
         if (eventMapper.unbookmark(id, userId) == 0) {
             throw new ResponseException(ErrorCode.UPDATE_DB_FAILED.getCode(), "Event not found or user not bookmark this event");
         }
+    }
+
+    @Override
+    public RecentEventVOs getRegisteredOngoingEvents(Integer pageNum, Integer pageSize) {
+        int userId = StpUtil.getLoginIdAsInt();
+        Page page = PageHelper.startPage(pageNum, pageSize);
+        List<RecentEventVO> events = eventMapper.getRegisteredOngoingEvents(userId);
+        RecentEventVOs recentEventVOS = new RecentEventVOs();
+        recentEventVOS.setEvents(events);
+        recentEventVOS.setPages(page.getPages());
+        return recentEventVOS;
+    }
+
+    @Override
+    public RecentEventVOs getRegisteredPastEvents(Integer pageNum, Integer pageSize) {
+        int userId = StpUtil.getLoginIdAsInt();
+        Page page = PageHelper.startPage(pageNum, pageSize);
+        List<RecentEventVO> events = eventMapper.getRegisteredPastEvents(userId);
+        RecentEventVOs recentEventVOS = new RecentEventVOs();
+        recentEventVOS.setEvents(events);
+        recentEventVOS.setPages(page.getPages());
+        return recentEventVOS;
+    }
+
+    @Override
+    public RecentEventVOs getViewedEvents(int pageNum, int pageSize) {
+        int userId = StpUtil.getLoginIdAsInt();
+        Page page = PageHelper.startPage(pageNum, pageSize);
+        List<RecentEventVO> events = eventMapper.getViewedEvents(userId);
+        RecentEventVOs recentEventVOS = new RecentEventVOs();
+        recentEventVOS.setEvents(events);
+        recentEventVOS.setPages(page.getPages());
+        return recentEventVOS;
+    }
+
+    @Override
+    public RecentEventVOs getBookmarkedEvents(int pageNum, int pageSize) {
+        int userId = StpUtil.getLoginIdAsInt();
+        Page page = PageHelper.startPage(pageNum, pageSize);
+        List<RecentEventVO> events = eventMapper.getBookmarkedEvents(userId);
+        RecentEventVOs recentEventVOS = new RecentEventVOs();
+        recentEventVOS.setEvents(events);
+        recentEventVOS.setPages(page.getPages());
+        return recentEventVOS;
     }
 
 
