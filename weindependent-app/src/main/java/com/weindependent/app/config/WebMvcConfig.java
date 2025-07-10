755bb2540e2468320dd1a5a497833469641d7d0f
Ching-Chieh-Wang <immactavish@gmail.com>
2025-07-10 14:33:59 -0400

    fix: move cors allow list to application.yaml

--- weindependent-app/src/main/java/com/weindependent/app/config/WebMvcConfig.java
+++ weindependent-app/src/main/java/com/weindependent/app/config/WebMvcConfig.java
@@ -1,20 +1,23 @@
 package com.weindependent.app.config;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 import java.util.Arrays;
+import java.util.List;
+
 @Configuration
 public class WebMvcConfig {
         //TODO: Origins need to be changed everytime frontend restart ngrok
+        @Value("${cors.allowed-origins}")
+        private List<String> allowedOrigins;
+
         @Bean
         public CorsFilter corsFilter() {
             CorsConfiguration config = new CorsConfiguration();
-            config.setAllowedOrigins(Arrays.asList(
-                    "http://localhost:5173",
-                    "https://5720-209-6-112-127.ngrok-free.app"
-            ));
+            config.setAllowedOrigins(allowedOrigins);
 
             config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
             config.setAllowedHeaders(Arrays.asList("*"));
